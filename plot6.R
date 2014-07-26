92702e59777440defa873a32ce05fadb79c581f4
CommonSenseLies <monica_barback@yahoo.com>
2014-07-26 10:45:54 -0400

    Graph 6 updates to look good in png

--- plot6.R
+++ plot6.R
@@ -15,7 +15,7 @@ SCCmotorvehiclebaltimorela$City <- factor(SCCmotorvehiclebaltimorela$City, label
 
 png("plot6.png",width=480,height=480)
 g<-ggplot(SCCmotorvehiclebaltimorela, aes(Year,SumEmissions,colour=City))
-g<-g + geom_line(aes(group=City),size=2)
+g<-g + geom_line(aes(group=City),size=1)
 g<-g + geom_hline(aes(yintercept=SCCmotorvehiclebaltimorela[1,3]),linetype=3,size=1)
 g<-g + geom_hline(aes(yintercept=SCCmotorvehiclebaltimorela[7,3]),linetype=3,size=1)
 g<-g + geom_hline(aes(yintercept=SCCmotorvehiclebaltimorela[2,3]),linetype=5,size=1)
@@ -23,7 +23,7 @@ g<-g + geom_hline(aes(yintercept=SCCmotorvehiclebaltimorela[8,3]),linetype=5,siz
 g<-g + geom_text(aes(2004, (SCCmotorvehiclebaltimorela[7,3] + SCCmotorvehiclebaltimorela[1,3])/2, label=as.character(as.integer(SCCmotorvehiclebaltimorela[7,3] - SCCmotorvehiclebaltimorela[1,3]))),color="black")
 g<-g + geom_text(aes(2004, (SCCmotorvehiclebaltimorela[2,3] + SCCmotorvehiclebaltimorela[8,3])/2, label=as.character(as.integer(SCCmotorvehiclebaltimorela[8,3] - SCCmotorvehiclebaltimorela[2,3]))),color="black")
 g<-g + geom_point(size=5)
-g<-g + ggtitle("Change in Emissions in Baltimore, MD vs. Los Angeles, CA due to Motor Vehicles")
+g<-g + ggtitle("Change in Emissions in Baltimore vs Los Angeles due to Motor Vehicles")
 
 print(g)
 dev.off()
